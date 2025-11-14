27a79471065637d7d95f4d73d7f18b62a974545a
dani <maildedani9@gmail.com>
2025-11-14 12:37:17 +0100

    wip

--- app/campus/page.tsx
+++ app/campus/page.tsx
@@ -1,18 +1,12 @@
 'use client';
 
-import RequireActive from "../auth/RequireActive";
-import RequireAuth from "../auth/RequireAuth";
 
 
 export default function DashboardPage() {
   return (
-    <RequireAuth>
-<RequireActive inactivePath="/campus/inactive">
         <div style={{ padding: 24, color: '#fff' }}>
           <h1>Dashboard</h1>
           <p>Contenido del campus…</p>
         </div>
-      </RequireActive>
-    </RequireAuth>
   );
 }
